c6152df449acc3fde902919cddac74fe3d107d7d
KhawanC <kauacassiano121@gmail.com>
2022-10-05 17:05:33 -0300

    adding feign client in payment api

---
+++ hr-payroll/src/main/java/com/kaua/hrpayroll/feignClient/WorkerFeign.java
@@ -0,0 +1,17 @@
+package com.kaua.hrpayroll.feignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.kaua.hrpayroll.dto.WorkerResponse;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8080", path = "/worker")
+public interface WorkerFeign {
+
+	@GetMapping("{id}")
+	ResponseEntity<WorkerResponse> findById(@PathVariable Long id);
+}
--- hr-payroll/src/main/java/com/kaua/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/kaua/hrpayroll/service/PaymentService.java
@@ -1,31 +1,21 @@
 package com.kaua.hrpayroll.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.kaua.hrpayroll.dto.WorkerResponse;
+import com.kaua.hrpayroll.feignClient.WorkerFeign;
 import com.kaua.hrpayroll.model.Payment;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	WorkerFeign workerFeign;
 
 	public Payment findPayment(Long workerId, Integer days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", "" + workerId);
 
-		WorkerResponse worker = restTemplate.getForObject(workerHost + "/worker/{id}", WorkerResponse.class,
-				uriVariables);
+		WorkerResponse worker = workerFeign.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
